b3390a17798cf39ad327a70992f7f9fbac1d413a
Rocio <rocio@dyn172-23-33-231.wireless.uwo.pri>
2026-01-10 13:48:39 -0500

    Added the stock price formula

--- src/snowflake/getStocks.py
+++ src/snowflake/getStocks.py
@@ -31,11 +31,18 @@ print(df.head())
 print(df.info())
 
 
-
-shares_per_home = 10
 df['fundamental_price_per_share'] = df['ESTIMATED_VALUE'] / shares_per_home
 
 print(df[['PROPERTY_ID', 'ESTIMATED_VALUE', 'fundamental_price_per_share']].head())
 conn.close()
 
+#we will tkae a user input from the shares of the home
+shares_per_home = 100  # Example fixed value, can be replaced with user input
+
+# create the stock price with some randomness 
+df['stock_price'] = df['fundamental_price_per_share'] / shares_per_home #add some randomness?
+
 df.to_csv('output_file.csv', index=False)
+
+
+
