45e0c5e82c9e60b5e1ad3a8cb07642749691a3f1
Rocio <rocio@dyn172-23-33-231.wireless.uwo.pri>
2026-01-10 13:17:18 -0500

    Got data from Snowflake

---
+++ getStocks.py
@@ -0,0 +1,41 @@
+# import snowflake.connector
+import snowflake.connector
+import pandas as pd
+from credentials import load_credentials
+
+print(snowflake.connector.__version__)
+
+creds = load_credentials()
+conn = snowflake.connector.connect(
+    account=creds['account'],
+    user=creds['user'],
+    password=creds['password'],
+    role=creds['role'],
+    warehouse=creds['warehouse'],
+    database=creds['database'],
+    schema=creds['schema'],
+)
+
+cur = conn.cursor()
+
+query = """
+SELECT PROPERTY_ID, ESTIMATED_VALUE
+FROM SECURE_SAMPLE_US_NATIONAL_SOLD
+WHERE ESTIMATED_VALUE IS NOT NULL
+LIMIT 1000
+"""
+
+df = pd.read_sql(query, conn)
+
+print(df.head())
+print(df.info())
+
+
+
+shares_per_home = 10
+df['fundamental_price_per_share'] = df['ESTIMATED_VALUE'] / shares_per_home
+
+print(df[['PROPERTY_ID', 'ESTIMATED_VALUE', 'fundamental_price_per_share']].head())
+conn.close()
+
+df.to_csv('output_file.csv', index=False)
